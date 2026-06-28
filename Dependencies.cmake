20354a4ff1557431bd6e859b26db609f692288d9
GIUSEPPE BIANCONI <gbianconi3@gmail.com>
2026-06-28 21:23:13 +0200

    build(cmake): bump deps, add SIMD probe, normalize formatting
    - Bump Catch2 from 3.12.0 to 3.14.0 in Dependencies.cmake
    - Bump CLI11 from 2.6.1 to 2.6.2 in Dependencies.cmake
    - Extract spdlog package setup into AddSpdlogPackage() helper in
      Dependencies.cmake; enable SPDLOG_ENABLE_PCH, SPDLOG_BUILD_PIC,
      and SPDLOG_WCHAR_SUPPORT/SPDLOG_WCHAR_FILENAMES on Windows builds
    - Include cmake/Simd.cmake in ProjectOptions.cmake and invoke
      check_all_simd_features() and print_simd_support() from
      myproject_global_options() so compiler SIMD capabilities are
      reported at configure time
    - Remove stray blank lines in src/sample_library/CMakeLists.txt
    - Strip leading blank line in .github/actions/setup_cache/action.yml
    - Reindent CMake files (CMakeLists.txt, Dependencies.cmake,
      ProjectOptions.cmake, fuzz_test/CMakeLists.txt,
      src/ftxui_sample/CMakeLists.txt,
      src/sample_library/CMakeLists.txt, test/CMakeLists.txt) to match
      clang-format style

--- Dependencies.cmake
+++ Dependencies.cmake
@@ -1,83 +1,93 @@
 include(cmake/CPM.cmake)
 
+function(AddSpdlogPackage WcharSupport WcharFilenames)
+    CPMAddPackage(
+            NAME spdlog
+            VERSION 1.17.0
+            GITHUB_REPOSITORY "gabime/spdlog"
+            OPTIONS
+            "SPDLOG_FMT_EXTERNAL ON"
+            "SPDLOG_ENABLE_PCH ON"
+            "SPDLOG_BUILD_PIC ON"
+            "SPDLOG_WCHAR_SUPPORT ${WcharSupport}"
+            "SPDLOG_WCHAR_FILENAMES ${WcharFilenames}"
+            "SPDLOG_SANITIZE_ADDRESS OFF"
+    )
+
+endfunction()
+
 # Done as a function so that updates to variables like
 # CMAKE_CXX_FLAGS don't propagate out to other
 # targets
 function(myproject_setup_dependencies)
 
-  # For each dependency, see if it's
-  # already been provided to us by a parent project
+    # For each dependency, see if it's
+    # already been provided to us by a parent project
 
-  if(NOT TARGET fmtlib::fmtlib)
-    cpmaddpackage(
-      NAME
-      fmt
-      GITHUB_REPOSITORY
-      "fmtlib/fmt"
-      GIT_TAG
-      "12.1.0"
-      SYSTEM
-      YES)
-  endif()
+    if (NOT TARGET fmtlib::fmtlib)
+        cpmaddpackage(
+                NAME
+                fmt
+                GITHUB_REPOSITORY
+                "fmtlib/fmt"
+                GIT_TAG
+                "12.1.0"
+                SYSTEM
+                YES)
+    endif ()
 
-  if(NOT TARGET spdlog::spdlog)
-    cpmaddpackage(
-      NAME
-      spdlog
-      VERSION
-      1.17.0
-      GITHUB_REPOSITORY
-      "gabime/spdlog"
-      SYSTEM
-      YES
-      OPTIONS
-      "SPDLOG_FMT_EXTERNAL ON")
-  endif()
+    if (NOT TARGET spdlog::spdlog)
+        if (WIN32)
+            AddSpdlogPackage(ON ON)
+        else ()
+            AddSpdlogPackage(OFF OFF)
+        endif ()
+    endif ()
 
-  if(NOT TARGET Catch2::Catch2WithMain)
-    cpmaddpackage(
-      NAME
-      Catch2
-      VERSION
-      3.12.0
-      GITHUB_REPOSITORY
-      "catchorg/Catch2"
-      SYSTEM
-      YES)
-  endif()
+    if (NOT TARGET Catch2::Catch2WithMain)
+        cpmaddpackage(
+                NAME
+                Catch2
+                VERSION
+                3.14.0
+                GITHUB_REPOSITORY
+                "catchorg/Catch2"
+                SYSTEM
+                YES)
+    endif ()
 
-  if(NOT TARGET CLI11::CLI11)
-    cpmaddpackage(
-      NAME
-      CLI11
-      VERSION
-      2.6.1
-      GITHUB_REPOSITORY
-      "CLIUtils/CLI11"
-      SYSTEM
-      YES)
-  endif()
+    if (NOT TARGET CLI11::CLI11)
+        cpmaddpackage(
+                NAME
+                CLI11
+                VERSION
+                2.6.2
+                GITHUB_REPOSITORY
+                "CLIUtils/CLI11"
+                SYSTEM
+                YES)
+    endif ()
 
-  if(NOT TARGET ftxui::screen)
-    cpmaddpackage(
-      NAME
-      FTXUI
-      VERSION
-      6.1.9
-      GITHUB_REPOSITORY
-      "ArthurSonzogni/FTXUI"
-      SYSTEM
-      YES)
-  endif()
+    if (NOT TARGET ftxui::screen)
+        cpmaddpackage(
+                NAME
+                FTXUI
+                VERSION
+                6.1.9
+                GITHUB_REPOSITORY
+                "ArthurSonzogni/FTXUI"
+                SYSTEM
+                YES)
+    endif ()
 
-  if(NOT TARGET tools::tools)
-    cpmaddpackage(
-      NAME
-      tools
-      GITHUB_REPOSITORY
-      "lefticus/tools"
-      GIT_TAG
-      "main")
-  endif()
+    if (NOT TARGET tools::tools)
+        cpmaddpackage(
+                NAME
+                tools
+                GITHUB_REPOSITORY
+                "lefticus/tools"
+                GIT_TAG
+                "main")
+    endif ()
 
 endfunction()
